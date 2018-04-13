e082c06d11f635f1ee2292da077c0468e1fdacba
ttian99 <176553611@qq.com>
2018-04-13 18:43:06 +0800

    增加资源压缩脚本

--- main.js
+++ main.js
@@ -1,29 +1,32 @@
 'use strict';
+var gulpTools = require('./lib/gulpfile');
 
 module.exports = {
-  load () {
+  load() {
     // execute when package loaded
     Editor.log('== res-min load ==');
+    console.log = Editor.log;
   },
 
-  unload () {
+  unload() {
     // execute when package unloaded
     Editor.log('== res-min unload ==');
   },
 
   // register your ipc messages here
   messages: {
-    'open' () {
+    'open'() {
       // open entry panel registered in package.json
       Editor.Panel.open('res-min');
     },
-    'say-hello' () {
+    'say-hello'() {
       Editor.log('Hello World!');
       // send ipc message to panel
       Editor.Ipc.sendToPanel('res-min', 'res-min:hello');
     },
-    'clicked' () {
+    'clicked'() {
       Editor.log('Button clicked!');
+      gulpTools();
     }
   },
 };
