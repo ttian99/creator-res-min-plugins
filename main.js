060b57791c7a78da73e03004430b598283e76e7d
ttian99 <176553611@qq.com>
2018-05-07 15:36:07 +0800

    完成面板可保存配置的功能

--- main.js
+++ main.js
@@ -1,34 +1,66 @@
 'use strict';
-var gulpTools = require('./lib/gulpfile');
+
+var fs = require('fs-extra');
+var task = require('./lib/task.js');
 
 module.exports = {
-  load() {
-    // execute when package loaded
-    Editor.log('== res-min load ==');
-    console.log = Editor.log;
+
+  load() { // execute when package loaded
+
   },
 
   unload() {
     // execute when package unloaded
-    Editor.log('== res-min unload ==');
   },
 
   // register your ipc messages here
   messages: {
+    // 打开面板
     'open'() {
-      // open entry panel registered in package.json
       Editor.Panel.open('res-min');
     },
-    'res-min'() {
-      Editor.log('Hello World!');
-      // send ipc message to panel
-      // Editor.Ipc.sendToPanel('res-min', 'res-min:hello');
-      Editor.log('res-min!');
-      gulpTools();
+    'qqplay'() {
+      Editor.log('qqplay!');
     },
-    'clicked'() {
-      Editor.log('Button clicked!');
-      gulpTools();
+    // 压缩图片
+    'image-min'(event, resPath) {
+      Editor.log('image-min 开始压缩');
+      let fileUrl = Editor.url('packages://res-min/config/config.json')
+      fs.readJson(fileUrl, (err, json) => {
+        if (err) return Editor.error(err);
+        Editor.info('get json success');
+        Editor.info(json);
+        var opts = json;
+        Editor.log('image-min 开始压缩 路径:' + resPath + ', 参数：' + JSON.stringify(opts));
+        task.resMin(resPath, opts, function () {
+          Editor.success('image-min 压缩完成');
+        });
+      });
+    },
+    // 保存配置信息
+    'save-config'(event, json) {
+      Editor.info('配置信息保存');
+      Editor.info(JSON.stringify(json));
+      var fileUrl = Editor.url('packages://res-min/config/config.json');
+      fs.writeJson(fileUrl, json, function (err) {
+          if (err) return Editor.error(err)
+          Editor.log('配置信息保存成功!');
+      });
+    },
+    // 编译完成的消息
+    'editor:build-finished'(event, arg) {
+      Editor.log('editor:build-finished');
+      fs.readJson(fileUrl, (err, json) => {
+        if (err) return Editor.error(err);
+        Editor.info('get json success');
+        Editor.info(json);
+        if (json.isAutoResMin) {
+          // 自动压缩 
+          Editor.Ipc.sendToMain('res-min:image-min', arg.dest);
+        } else {
+          Editor.log('自动压缩配置为false!!');
+        }
+      });
     }
-  },
+  }
 };
--- panel/index.js
+++ panel/index.js
@@ -1,37 +1,90 @@
-// panel/index.js, this filename needs to match the one registered in package.json
+let fs = require('fs-extra');
+let path = require('path');
+
+// 引入electron的对话框: https://electronjs.org/docs/api/dialog
+let { dialog } = require('electron').remote;
+
+let cfg = {};
+
 Editor.Panel.extend({
-  // css style for panel
   style: `
-    :host { margin: 5px; }
-    h2 { color: #f90; }
+    :host {
+      margin: 10px;
+    }
+
+    .quality: {
+      width: 40px;
+    }
   `,
 
-  // html template for panel
   template: `
-    <h2>res-min</h2>
-    <hr />
-    <div>State: <span id="label">--</span></div>
-    <hr />
-    <ui-button id="btn">Send To Main</ui-button>
+  
+  <h2>res-min</h2>
+  <hr />
+  <h3>压缩资源配置</h3>
+  <br />
+  <input type="checkbox" id="checkbox" v-model="isAutoResMin" @change="onCheckBox1">
+  <label for="checkbox">自动压缩{{ isAutoResMin }}</label>
+  <br />
+  <input type="checkbox" id="checkbox" v-model="cache" @change="onCheckBox2">
+  <label for="checkbox">启用缓存{{ cache }}</label>
+  <br />
+  <label for="text">设置png格式图片的品质(1-100):</label>
+  <input class="quality" type="text" v-model="pngQuality"></input>
+  <br />
+  <label for="text">设置jpg格式图片的品质(1-100):</label>
+  <input class="quality" type="text" v-model="jpgQuality"></input>
+  <br />
+  <hr />
+  <button id="btn" @click=onBtnClick>保存配置</button>
+  <hr />
   `,
 
-  // element and variable binding
-  $: {
-    btn: '#btn',
-    label: '#label',
-  },
+  ready() {
+    Editor.log('ready');
+    let fileUrl = Editor.url('packages://res-min/config/config.json')
+    fs.readJson(fileUrl, (err, json) => {
+      if (err) return Editor.error(err);
+      Editor.info('get json success');
+      Editor.info(JSON.stringify(json));
 
-  // method executed when template and styles are successfully loaded and initialized
-  ready () {
-    this.$btn.addEventListener('confirm', () => {
-      Editor.Ipc.sendToMain('res-min:clicked');
+      cfg = json;
+
+      new window.Vue({
+        el: this.shadowRoot,
+        data: {
+          isAutoResMin: json.isAutoResMin,
+          jpgQuality: json.jpgQuality,
+          pngQuality: json.pngQuality,
+          cache: json.cache
+        },
+        methods: {
+          // 数据变化时的
+          onCheckBox1: function () {
+            // Editor.info('isAutoResMin = ' + this.isAutoResMin);
+          },
+          onCheckBox2: function () {
+            // Editor.info('cache = ' + this.cache);
+          },
+          onBtnClick: function () {
+            var json = {
+              isAutoResMin: this.isAutoResMin,
+              jpgQuality: this.jpgQuality,
+              pngQuality: this.pngQuality,
+              cache: this.cache
+            };
+            Editor.info('save config: ' + JSON.stringify(json));
+            Editor.Ipc.sendToMain('res-min:save-config', json);
+          }
+        }
+      });
     });
   },
-
-  // register your ipc messages here
+  // 关闭面板保存配置
+  close() {
+    // Editor.Ipc.sendToMain('res-min:save-config', config)
+  },
   messages: {
-    'res-min:hello' (event) {
-      this.$label.innerText = 'Hello!';
-    }
+
   }
 });
